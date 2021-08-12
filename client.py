17e2d071e32884efcabf5dcbc28eb5b476f1dcd8
Andreé Toledo <edgar.andree20@gmail.com>
2021-08-12 02:47:59 -0600

    Update client.py

--- client.py
+++ client.py
@@ -10,6 +10,15 @@ import base64
 import datetime
 import mimetypes
 
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout
+from xml.etree import cElementTree as ET
+from sleekxmpp.plugins.xep_0004.stanza.field import FormField, FieldOption
+from sleekxmpp.plugins.xep_0004.stanza.form import Form
+from sleekxmpp.plugins.xep_0047.stream import IBBytestream
+from consts import OKGREEN, OKBLUE, WARNING, FAIL, ENDC, BLUE, RED, NEW_MESSAGE, FILE_OFFER, SUSCRIPTION, GOT_ONLINE, error_msg, GROUPCHAT, STREAM_TRANSFER
+
+
 DIRNAME = os.path.dirname(__file__)
 
 
@@ -59,3 +68,149 @@ class Client(ClientXMPP):
 
         self.received = set()
         self.presences_received = threading.Event()
+    
+    def session_start(self):
+        try:
+            self.get_roster(block=True)
+        except IqError as err:
+            print('Error: %s' % err.iq['error']['condition'])
+        except IqTimeout:
+            print('Error: Request timed out')
+
+        self.send_presence()
+
+    # Create a new user dict
+    def create_user_dict(self, wait=False):
+
+        try:
+            self.get_roster(block=True)
+        except IqError as err:
+            print('Error: %s' % err.iq['error']['condition'])
+        except IqTimeout:
+            print('Error: Request timed out')
+
+        groups = self.client_roster.groups()
+        for jid in groups['']:
+            # Check we are not evaluating ourselves or a conference room
+            if jid == self.boundjid.bare or 'conference' in jid:
+                continue
+
+            # Get some data
+            sub = self.client_roster[jid]['subscription']
+            name = self.client_roster[jid]['name']
+            username = str(jid.split('@')[0])
+            connections = self.client_roster.presence(jid)
+
+            # Check if connections is empty
+            if connections.items():
+                # Go through each connection
+                for res, pres in connections.items():
+
+                    show = 'available'
+                    status = ''
+                    if pres['show']:
+                        show = pres['show']
+                    if pres['status']:
+                        status = pres['status']
+
+                    # Check if the user is in the dict, else add it
+                    if not jid in self.contact_dict:
+                        self.contact_dict[jid] = User(
+                            jid, name, show, status, sub, username, res)
+                    else:
+                        self.contact_dict[jid].update_data(
+                            status, show, res, sub)
+
+            # User is not connected, still add him to the dict
+            else:
+                self.contact_dict[jid] = User(
+                    jid, name, 'unavailable', '', sub, username, '')
+
+    # Returns a dict jid - User. If it's empty, create it.
+    def get_user_dict(self):
+        if not self.contact_dict:
+            self.create_user_dict()
+        return self.contact_dict
+
+    # Create user dict on new presence
+    def wait_for_presences(self, pres):
+        self.received.add(pres['from'].bare)
+        if len(self.received) >= len(self.client_roster.keys()):
+            self.presences_received.set()
+        else:
+            self.presences_received.clear()
+
+        self.create_user_dict()
+
+    # Act upon a received message
+    def received_message(self, msg):
+
+        sender = str(msg['from'])
+        jid = sender.split('/')[0]
+        username = jid.split('@')[0]
+        if msg['type'] in ('chat', 'normal'):
+            print(f'{BLUE}{NEW_MESSAGE} New message from {jid}{ENDC}')
+
+            if not jid in self.contact_dict:
+                self.contact_dict[jid] = User(
+                    jid, '', '', '', '', username)
+
+            self.contact_dict[jid].add_message_to_list((username, msg['body']))
+
+        elif msg['type'] in ('groupchat', 'normal'):
+            nick = sender.split('/')[1]
+
+            # don't let you get a notification from yourself
+            if jid in self.room_dict:
+                self.room_dict[jid].add_message_to_list((nick, msg['body']))
+                if nick != self.room_dict[jid].nick:
+                    print(
+                        f'{BLUE}{GROUPCHAT} New message from {nick} in {jid}{ENDC}')
+
+    def request_si(self, user_jid, file_path):
+
+        file_path = file_path.replace('\\', '/')
+        # Get some  data from the file
+        file_name = file_path.split('/')[-1]
+        file_size = os.path.getsize(file_path)
+        unix_date = os.path.getmtime(file_path)
+        file_date = datetime.datetime.utcfromtimestamp(
+            unix_date).strftime('%Y-%m-%dT%H:%M:%SZ')
+        file_mime_type = 'not defined'
+
+        try:
+            file_mime_type = str(
+                mimetypes.MimeTypes().guess_type(file_path)[0])
+        except:
+            pass
+
+        data = None
+        # Read the contents of the file and encode it to b64
+        with open(file_path, 'rb') as file:
+            data = base64.b64encode(file.read()).decode('utf-8')
+
+        # Set the data of the request
+        dest = self.contact_dict[user_jid].get_full_jid()
+
+        try:
+
+            req = self.plugin['xep_0096'].request_file_transfer(
+                jid=dest,
+                name=file_name,
+                size=file_size,
+                mime_type=file_mime_type,
+                sid='ibb_file_transfer',
+                desc='Envío un archivo con descripción',
+                date=file_date
+            )
+
+            # Wait for the other user to accept the file transfer
+            print(f'{BLUE}{FILE_OFFER} Offering a file transfer to the user.{ENDC}')
+            time.sleep(2)
+
+            # Open the ibb stream transfer
+            stream = self.plugin['xep_0047'].open_stream(
+                jid=dest, sid='ibb_file_transfer', ifrom=self.boundjid.full)
+
+            # Wait for the other client to get notified about this
+            time.sleep(2)
