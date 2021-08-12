10cbfc86282d92c3ace019757f81bda95d74beb0
Andreé Toledo <edgar.andree20@gmail.com>
2021-08-12 02:55:29 -0600

    main agregado

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 import os
 import time
 import logging
+import sleekxmpp
+
+from getpass import getpass
+from prettytable import PrettyTable
+from sleekxmpp.exceptions import IqError, IqTimeout
 
 import client
 from consts import *
